46f939045e78ebe0bd288136d39a730477d53029
EstebanFlorence <asap.bacco@protonmail.com>
2024-09-02 16:27:46 +0200

    Scripts Update

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,10 @@
 async function main ()
 {
-    // Contract to deploy
-    const MySimpleStorage = await ethers.getContractFactory('MySimpleStorage');
-    console.log('Deploying MySimpleStorage...');
-    const mysimplestorage = await MySimpleStorage.deploy();
-    await mysimplestorage.waitForDeployment();
-    console.log('MySimpleStorage deployed to:', await mysimplestorage.getAddress());
+    const ScoreBoard = await ethers.getContractFactory('ScoreBoard');
+    console.log('Deploying ScoreBoard...');
+    const scoreboard = await ScoreBoard.deploy();
+    await scoreboard.waitForDeployment();
+    console.log('ScoreBoard deployed to:', await scoreboard.getAddress());
 }
 
 main()
--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,23 @@ const { ethers } = require("hardhat");
 async function main ()
 {
     // Retrieve accounts from the local node
-    // const provider = new ethers.JsonRpcProvider();
-    // const accounts = await provider.listAccounts();
-    // console.log(accounts);
+    const provider = new ethers.JsonRpcProvider();
+    const accounts = await provider.listAccounts();
+    console.log(accounts);
 
-    // Set up ethers contract, representing our deployed MySimpleStorage instance
+    // Set up ethers contract, representing our deployed ScoreBoard instance
     const address = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
-    const MySimpleStorage = await ethers.getContractFactory('MySimpleStorage');
-    const mysimplestorage = MySimpleStorage.attach(address);
+    const ScoreBoard = await ethers.getContractFactory('ScoreBoard');
+    const scoreboard = ScoreBoard.attach(address);
 
-    // Send a transaction to store() a new value in the MySimpleStorage
-    await mysimplestorage.store(42);
+    // Call a function
+    const score = await scoreboard.addPlayer(accounts[0], "Index", 42);
+    const player = await scoreboard.getPlayer(accounts[0]);
+    console.log("Player details: ", player);
 
-    // Call the retrieve() function of the MySimpleStorage contract
-    const value = await mysimplestorage.retrieve();
-    console.log('MySimpleStorage value is', value.toString());
+    const rankings = await scoreboard.getRankings();
+    console.log("Rankings :", rankings);
+
 }
 
 main()
