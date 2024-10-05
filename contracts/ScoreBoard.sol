4475017bb84c63c470b0be2102e43393364b0a11
EstebanFlorence <77881363+EstebanFlorence@users.noreply.github.com>
2024-10-05 20:16:40 +0200

    ScoreBoard Update

--- contracts/ScoreBoard.sol
+++ contracts/ScoreBoard.sol
@@ -11,154 +11,188 @@ contract ScoreBoard is Ownable
 {
 	struct Player
 	{
+		uint256	id;
 		string	name;
 		uint256	score;
 		bool	exists;
+		bool	isWinner;
 	}
 
 	struct Game
 	{
-		Player player1;
-		Player player2;
+		Player	player1;
+		Player	player2;
 	}
 
 	struct Tournament
 	{
-		string	name;
-		Game[]	rounds;
-		uint256	numPlayers;
-		mapping (address => Player) players;
+		uint256		date;
+		uint256		id;
+		string		name;
+		uint256		numPlayers;
+		Player[]	players;
+		Game[][]	rounds;
 	}
 
-	mapping (address => Player) private	players;
-	Tournament[]		tournaments;	// private?
-	address[] public	rankings;
-	uint256 public		numPlayers;
-	uint256 private		addressCounter;
+	uint256 public		_numTournaments;
+	Tournament[]		_tournaments;	// private?
+	Player[] public		_rankings;
+	uint256 private		_addressCounter;
 
 	constructor() Ownable(msg.sender) {}
 
-	event PlayerAdded(address player);
-	event PlayerRemoved(address player);
+	event	TournamentAdded(string name);
+	event	RankingsUpdated(string name);
 
-
-	function startTournament(address[] memory _addresses, string[] memory _names) public onlyOwner	// calldata?
+	function addTournament(string calldata name, string[] calldata participants, string[] calldata allPlayers, uint256[] calldata allScores) public onlyOwner
 	{
-		require (_addresses.length == _names.length, "Addresses and players arrays must have the same lenght");
+		require(allPlayers.length == allScores.length, "Players and Scores lenght mismatch");
 
-		for (uint256 i = 0; i < _addresses.length; ++i)
+		// Tournament
+		Tournament storage	newTournament = _tournaments.push();
+		newTournament.id = _numTournaments++;
+		newTournament.name = name;
+		newTournament.date = block.timestamp;
+		newTournament.numPlayers = participants.length;
+
+		// Players
+		for (uint i = 0; i < participants.length; ++i)
 		{
-			address playerAddress = _addresses[i];
-			if (playerAddress == address(0))
-				playerAddress = generateNewAddress();
-			addPlayer(playerAddress, _names[i], 0);
+			Player memory	newPlayer = Player
+			({
+				id: i + 1,
+				name: participants[i],
+				score: 0,
+				exists: true,
+				isWinner: false
+			});
+			newTournament.players.push(newPlayer);
 		}
+
+		// Rounds and Games
+		uint256	numRounds = getNumRounds(participants.length);
+		for (uint256 r = 0; r < numRounds; ++r)
+		{
+			uint256	numGames = getNumGames(r, participants.length);
+			// Game[] memory	round = new Game[](numGames);
+			// newTournament.rounds.push(round);
+			newTournament.rounds.push();
+			for (uint256 g = 0; g < numGames; ++g)
+			{
+				newTournament.rounds[g].push();
+			}
+		}
+		addGames(newTournament, allPlayers, allScores);
+		setWinner(newTournament.id);
 	}
 
-	// pseudo-address, generate real ones outside
-	function generateNewAddress() private returns (address)
+	function setWinner(uint256 tournamentId) internal
 	{
-		addressCounter++;
-		return address(uint160(addressCounter));
-	}
+		Tournament storage	tournament = _tournaments[tournamentId];
+		uint256	lastRound = tournament.rounds.length - 1;
+		uint256	g = tournament.rounds[lastRound].length - 1;	// Or 0?
+		Game storage	finalGame = tournament.rounds[lastRound][g];
+		Player storage		winner = finalGame.player1.score > finalGame.player2.score ?
+									finalGame.player1 : finalGame.player2;
 
-	function getNewAddress() public returns (address)
-	{
-		return generateNewAddress();
-	}
-
-	function addGame(address _player1, address _player2) public onlyOwner
-	{
-
-	}
-
-	function addPlayer(address _player, string memory _name, uint256 _score) public onlyOwner
-	{
-		require(!players[_player].exists, "Player already exists");
-
-		players[_player] = Player(_name, _score, true);
-		rankings.push(_player);
-		++numPlayers;
+		_rankings.push(winner);
 		// updateRankings();
-
-		emit PlayerAdded(_player);
+		emit RankingsUpdated(winner.name);
 	}
 
-	function removePlayer(address _player) public onlyOwner
+	function addGames(Tournament storage newTournament, string[] calldata allPlayers, uint256[] calldata allScores) internal //pure
 	{
-		require(players[_player].exists, "Player does not exist");
+		uint256	playerIndex = 0;
+		// Rounds
+		for (uint256 r = 0; r < newTournament.rounds.length; ++r)
+		{
+			Game[] storage	round = newTournament.rounds[r];
+			// Games
+			for (uint256 g = 0; g < round.length; ++g)
+			{
+				Game storage	game = round[g];
 
-		// rankings to contain only the tournaments' winners
-		// uint index = findPlayerIndex(_player);
+				// game.player1 = getPlayer(newTournament.players, allPlayers[playerIndex]);
+				// game.player2 = getPlayer(newTournament.players, allPlayers[playerIndex + 1]);
 
-		// require(index < rankings.length, "Player not found in rankings");
+				// game.player1.score = allScores[playerIndex];
+				// game.player2.score = allScores[playerIndex + 1];
 
-		// for (uint i = index; i < rankings.length - 1; ++i)
-		// {
-		// 	rankings[i] = rankings[i + 1];
-		// }
+				game.player1 = Player
+				({
+					id: playerIndex + 1,
+					name: allPlayers[playerIndex],
+					score: allScores[playerIndex],
+					exists: true,
+					isWinner: false
+				});
 
-		// rankings.pop();
-		delete players[_player];
-		--numPlayers;
-		
-		emit PlayerRemoved(_player);
+				game.player2 = Player
+				({
+					id: playerIndex + 2,
+					name: allPlayers[playerIndex + 1],
+					score: allScores[playerIndex + 1],
+					exists: true,
+					isWinner: false
+				});
+
+				Player storage	winner = game.player1.score > game.player2.score ? game.player1 : game.player2;
+				winner.isWinner = true;
+
+				playerIndex += 2;
+			}
+		}
+		emit TournamentAdded(newTournament.name);
 	}
 
-	// function findPlayerIndex(address _player) internal view returns (uint)
-	// {
-	// 	for (uint i = 0; i < rankings.length; ++i)
-	// 	{
-	// 		if (rankings[i] == _player)
-	// 			return i;
-	// 	}
-
-	// 	return rankings.length;
-	// }
-
-	function updateScore(address _player, uint256 _score) public onlyOwner
+	function getPlayer(Player[] storage players, string calldata name) internal view returns (Player storage)
 	{
-		require(players[_player].exists, "Player does not exist");
+		for (uint256 i = 0; i < players.length; i++)
+		{
+			if (keccak256(abi.encodePacked(players[i].name)) == keccak256(abi.encodePacked(name)))
+				return players[i];
+		}
+		revert("Player not found");
+	}
 
-		require(players[_player].score < _score, "Update only higher score");
+	function getNumRounds(uint256 numPlayers) internal pure returns (uint256)
+	{
+		uint256	rounds = 0;
+		while (numPlayers > 1)
+		{
+			numPlayers /= 2;
+			rounds++;
+		}
+		return rounds;
+	}
 
-		players[_player].score = _score;
-
-		updateRankings();
+	function getNumGames(uint256 roundIndex, uint256 numPlayers) internal pure returns( uint256)
+	{
+		return numPlayers / (2 ** (roundIndex + 1));
 	}
 
 	function updateRankings() public onlyOwner
 	{
-		// Sort.quickSort(players, rankings, 0, rankings.length - 1);
+		// Sort.quickSort(players, _rankings, 0, _rankings.length - 1);
 
-		Sort.insertionSort(players, rankings);
+		// Sort.insertionSort(players, _rankings);
 
-		// for (uint256 i = 0; i < rankings.length; ++i)
+		// for (uint256 i = 0; i < _rankings.length; ++i)
 		// {
-		// 	players[rankings[i]].ranking = i + 1;
+		// 	players[_rankings[i]].ranking = i + 1;
 		// }
 	}
 
-	function getPlayer(address _player) public view returns (string memory name, uint256 score)
+	function getRankings() public view returns (Player[] memory)
 	{
-		require(players[_player].exists, "Player does not exist");
-		Player memory player = players[_player];
-		return(player.name, player.score);
+		return _rankings;
 	}
 
-	function getRankings() public view returns (address[] memory)
+	function getTournaments() public view returns (Tournament[] memory)
 	{
-		return rankings;
-	}
+		require(_tournaments.length > 0, "No Tournaments available");
 
-	function getNumPlayers() public view returns (uint256)
-	{
-		return numPlayers;
-	}
-
-	function getWinner() public view returns (address winner)
-	{
-		// require()
+		return _tournaments;
 	}
 
 }
